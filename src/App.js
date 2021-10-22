65aaf1868a1e678e1aa06f61838471ff092cda2e
ryuga <vaishnav2353@gmail.com>
2021-10-22 16:59:35 +0530

    Add about page template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Index from "./pages/Index";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
+import Index from "./pages/Index";
+import About from "./pages/About";
 
 function App() {
   return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
-            <Index />
-          </Route>
+          <Route exact path="/" component={Index}/>
+          <Route path="/events" component={Index}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Index}/>
         </Switch>
       </BrowserRouter>
   );
