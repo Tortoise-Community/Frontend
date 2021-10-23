9d0c98833fcf3dc536ce39aa20cd64fa3c575755
ryuga <vaishnav2353@gmail.com>
2021-10-23 15:30:32 +0530

    Add Project section

---
+++ src/sections/ProjectSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ProjectItem from "../components/ProjectItem";
+import Section from "../components/Section";
+
+export default function ProjectSection() {
+   return (
+       <Section title={"Projects"}>
+           <div className="row g-4">
+                <ProjectItem/>
+                <ProjectItem/>
+                <ProjectItem/>
+           </div>
+       </Section>
+   )
+}
