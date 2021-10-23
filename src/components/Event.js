690d7972f189025bda548afb34c0a72644878bbe
qt-coder <chuchenkhang@gmail.com>
2021-10-23 17:21:05 +0800

    Rudimentary event page done

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles/Event.css'
 
 function Event({title, slug, desc}) {
     return (
-        <div>
-            {title}
-        </div>
+        <Link href={`/events/${slug}`}>
+            <div className='flex container-fluid event mb-5 p-5 m-l-0 m-r-0'>
+                <h1>{title}</h1>
+                <p>{desc}</p>
+            </div>
+        </Link>
     )
 }
 
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -10,17 +10,23 @@ function Events() {
         {
             title: 'some title',
             slug: 'fake-event',
-            desc: 'some desc',
+            desc: 'lorem ipsum dolor sit amet and more words to pad out the word count because vscodes lorem ipsum isnt working',
             id: 1
+        },
+        {
+            title: 'some title',
+            slug: 'fake-event-2',
+            desc: 'lorem ipsum dolor sit amet and more words to pad out the word count because vscodes lorem ipsum isnt working',
+            id: 2
         }
     ])
 
     return (
         <div>
-            <Header>
-                <div style={{ color: 'white' }} className='white'>
-                    <h1>Events</h1>
-                    <div>
+            <Header >
+                <div style={{ color: 'white' }} className='white p-5'>
+                    <h1 className='display-4 mb-2'>Events</h1>
+                    <div className='flex container-fluid mt-5'>
                         {/* Event Mapping */}
                         {fakeEvents.map((fakeEvent) => (
                             <Event
