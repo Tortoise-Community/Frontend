191f874d6ddf012e10ef174af5267bf5a6ce6212
ryuga <vaishnav2353@gmail.com>
2021-10-24 19:10:35 +0530

    Add tags and button

--- src/components/Event.js
+++ src/components/Event.js
@@ -6,9 +6,15 @@ export default function Event({title, slug, desc}) {
     return (
         <Link className="event-box border border-1" to={`/events/${slug}`}>
             <div className="event-body">
-                <h4>Halloween Codejam</h4>
-                <span className="date">Oct.20.2021 - Oct.31.2021</span>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <div className="col">
+                    <h4>Halloween Codejam</h4>
+                    <span className="date">Oct.20.2021 - Oct.31.2021</span>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
+                <div className="col">
+                    <a className={"category"}>#CTF</a>
+                    <a className={"category"}>#Programming</a>
+                </div>
             </div>
             <div className="event-footer">
                 <div className="col">
@@ -21,6 +27,7 @@ export default function Event({title, slug, desc}) {
                         <p>Discord Nitro X 3 Months, 1 Core VPS</p>
                     </div>
                 </div>
+                <button className="btn btn-default text-uppercase">Participate <i className="fas fa-arrow-right"/></button>
             </div>
         </Link>
     )
