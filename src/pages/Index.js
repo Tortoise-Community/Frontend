9bb2ced04fdfea890431c95193e09dce8153d593
ryuga <vaishnav2353@gmail.com>
2021-10-29 00:26:17 +0530

    Hide pages for a minimal version of the site

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Section from "../components/Section"
 import ProjectSection from "../sections/ProjectSection";
 
 function Index() {
@@ -10,9 +9,8 @@ function Index() {
                 <div className="container">
                     <div className="row">
                         <div className="col-xl-4 col-lg-4">
-
                         </div>
-                        <div className="col-xl-7 col-lg-8 p-4">
+                        <div className="col-xl-7 col-lg-8 p-4 order-1">
                             <h1 className="text-uppercase front-header m-2">Tortoise Python Community</h1><br/>
                             <p className="front-quote m-2">"We called him Tortoise because he taught us!"</p>
                             <button className="btn btn-outline-primary m-2">Join now <i className="bi bi-arrow-right-short"/></button>
