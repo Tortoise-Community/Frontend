6354b0338ff445d61d99ea77a2e0b3391837997b
ryuga <vaishnav2353@gmail.com>
2021-11-08 22:52:42 +0530

    Add syntax highlight overlay

--- src/pastebin/components/TextEditor.js
+++ src/pastebin/components/TextEditor.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import "./styles/TextEditor.scss";
 
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 export default function TextEditor({editable}) {
 
     const [numberBlocks, setNumberBlocks] = useState([1]);
+    const [body, setBody] = useState('');
 
     function EditorLine({numberBlocks}){
         return (
@@ -14,6 +18,7 @@ export default function TextEditor({editable}) {
     }
 
     const bodyHandler = value => {
+            setBody(value);
             let lineBreaks = value.match(/\n/gi) || [];
             let numOfLines = lineBreaks.length ? lineBreaks.length + 1 : 1;
             setNumberBlocks(Array.from({length: numOfLines}, (_, i) => i + 1))
@@ -24,7 +29,10 @@ export default function TextEditor({editable}) {
         <div className="paste-bin">
                 <div className="editor">
                     <EditorLine numberBlocks={numberBlocks}/>
-                    <textarea className="w-100" onChange={e => bodyHandler(e.target.value)}/>
+                    <textarea className="w-100 text-input" spellCheck="false" onChange={e => bodyHandler(e.target.value)}/>
+                       <SyntaxHighlighter style={atomOneDark} className="text-output">
+                          {body}
+                        </SyntaxHighlighter>
                 </div>
         </div>
     )
