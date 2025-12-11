768422248776281e174ac154082480219f1c5b8d
Ryuga <vaishnav2353@gmail.com>
2025-12-11 12:53:30 +0530

    update events

--- src/web/pages/Events.js
+++ src/web/pages/Events.js
@@ -15,6 +15,19 @@ export default function Events() {
   const [Events, setEvents] = useState([
     {
       pk: 0,
+      slug: "hata-code-jam-2020",
+      name: "Tortoise Hata Code Jam",
+      short_desc:
+        "Python code jam using Hata library as the selected technology",
+      host_name: "@HuyaneMatsu, @Braindead, @Ryuga",
+      winner_name: "Team Maple",
+      prize: "23+ Discord Nitros, 3X VPS for 3 months, 3X AWS RDS 1 year",
+      due_date: "21/10/20",
+      end_date: "20/11/20",
+      event_tags: ["#coding", "#hata", "#codejam", "#discord-api"],
+    },
+    {
+      pk: 1,
       slug: "halloween-coding-event-2019",
       name: "Tortoise Halloween Coding Event ",
       short_desc: "Halloween coding event for the Tortoise Discord server",
@@ -28,12 +41,13 @@ export default function Events() {
   ]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const events = await api.getEvents();
-      setEvents(events.data);
-      setLoading(false);
-    };
-    fetchData();
+    // const fetchData = async () => {
+    //   const events = await api.getEvents();
+    //   setEvents(events.data);
+    //   setLoading(false);
+    // };
+    // fetchData();
+    setLoading(false);
   }, []);
   return (
     <div>
